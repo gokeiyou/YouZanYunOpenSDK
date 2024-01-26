6cf885d4b967f66b0911ce352751c76ab817b681
Jimmy Wu <wjy307@qq.com>
2024-01-26 17:25:16 +0800

    添加注释
    Signed-off-by: Jimmy Wu <wjy307@qq.com>

--- YouZanYunOpenSDK/Api/Models/Response/Customer/ScrmSouGouMemberJoinLinkResponse.cs
+++ YouZanYunOpenSDK/Api/Models/Response/Customer/ScrmSouGouMemberJoinLinkResponse.cs
@@ -7,20 +7,51 @@ namespace YouZan.Open.Api.Entry.Response.Customer
     /// </summary>
     public class ScrmSouGouMemberJoinLinkResponse
     {
+        /// <summary>
+        /// 分享详情
+        /// </summary>
         [JsonProperty("share_detail")]
         public string ShareDetail { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
         [JsonProperty("title")]
         public string Title { get; set; }
+
+        /// <summary>
+        /// 分享图标
+        /// </summary>
         [JsonProperty("share_icon")]
         public string ShareIcon { get; set; }
+
+        /// <summary>
+        /// 类别ID
+        /// </summary>
         [JsonProperty("classid")]
         public string ClassId { get; set; }
+
+        /// <summary>
+        /// URL链接
+        /// </summary>
         [JsonProperty("url")]
         public string Url { get; set; }
+
+        /// <summary>
+        /// 图片
+        /// </summary>
         [JsonProperty("image")]
         public string Image { get; set; }
+
+        /// <summary>
+        /// 信息
+        /// </summary>
         [JsonProperty("info")]
         public string Info { get; set; }
+
+        /// <summary>
+        /// 分享标题
+        /// </summary>
         [JsonProperty("share_title")]
         public string ShareTitle { get; set; }
     }
